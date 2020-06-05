942eb68c60e3b5fd9158057ff3750fdf1d1c2c6d
omar <omarasmar9955@gmail.com>
2020-06-05 13:18:17 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,10 +11,13 @@ $(document).ready(function(){
     autoplay: true,
   });
 
-
-  
-
 });
 
 
-
+var fileInput = document.querySelector('.custom-file-input');
+var fileNamePlace = document.querySelector('#file-name')
+// console.log();
+fileInput.addEventListener('change', ()=>{
+  var fileName = fileInput.files[0].name;
+  fileNamePlace.textContent = fileName;
+})
--- assets/js/slickslidertabs.js
+++ assets/js/slickslidertabs.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
   "use strict";
-
-  $('.project-slider').slick({
+  $('#project-slider-1').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
-    asNavFor: '.pro-det-slider',
+    centerMode: false,
+    asNavFor: '#pro-det-slider-1',
     autoplay: false,
+    focusOnSelect: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 3,
-          infinite: true,
         }
       },
       {
@@ -35,18 +35,61 @@ $(document).ready(function () {
   });
 
 
-  $('.pro-det-slider').slick({
+  $('#pro-det-slider-1').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    asNavFor: '.project-slider',
+    asNavFor: '#project-slider-1',
     dots: false,
     fade: true,
     centerMode: true,
-    focusOnSelect: true,
     autoplay: true,
     infinite: true,
   });
 
 
+  $('#project-slider-2').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: false,
+    asNavFor: '#pro-det-slider-2',
+    autoplay: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
+
+  $('#pro-det-slider-2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '#project-slider-2',
+    dots: false,
+    fade: true,
+    centerMode: true,
+    autoplay: true,
+    infinite: true,
+  });
 });
